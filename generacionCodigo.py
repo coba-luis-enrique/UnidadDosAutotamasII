cb7f670115f22c2b362da675bbcf7a88065fde2e
Luis Coba <cobaluisenrique@gmail.com>
2020-03-24 14:19:58 -0600

    correcionInstrucciones

--- generacionCodigo.py
+++ generacionCodigo.py
@@ -1,14 +1,18 @@
 import re
 
-print("SOLO TRABAJA CON UN VALOR O CARACTER")
-print("LA EXPRESION DEBE EMPEZAR CON X = ")
 print("UNIDAD DOS AUTOMATAS")
 print("GENERACION DE CODIGO INTERMEDIO")
-print("CASO 1: EJEMPLO  c") #EXPRESIONES INVALIDAS X= 2 - 5 + Y ó X = 2 * 5 / Y
-print("CASO 2: EJEMPLO  X = a / a + b * b") #EXPRESIONES INVALIDAS SERIAN LAS QUE NO SEAN DE MULTIPLICACION
+print("SOLO TRABAJA CON UN VALOR O CARACTER")
+print("LA EXPRESION DEBE EMPEZAR CON : X= ")
+print("SOLO EXPRESIONES VALIDAS COMO X = 2 + 4 * Y")
+print("CASO 1: EJEMPLO  X = 2 + 5 * y") 
+print("CASO 2: EJEMPLO  X = a / a + b * b") 
 print("CASO 3: EJEMPLO  X= (a+ 2) / 3 + b")
 print("CASO 4: EJEMPLO  X = (a+ 2) / (3 - b)")
-x=int(input("Seleccione una opcion"))
+x=int(input("Seleccione una opcion: "))
+
+# LOS CASOS SON SIN REGEX SOLO CON LISTAS, POR ESO, SE TRABAJA SOLO CON UN DIGITO O UN SOLO
+#CARACTER
 
 if x==1:
     #CASO UNO SIN USO DE REGEX, CON LISTAS.
@@ -29,7 +33,7 @@ if x==1:
             p.remove(p[suma]) # SE ELIMINA "*"
             p.remove(p[suma-1]) #SE ELIMINA EL "5"
             p.remove(p[suma-1]) #SE ELIMINA EL "Y"
-            #QUE P = [X,=,2,+]
+
 
     temporalUno = ""
     for i in p:
@@ -56,8 +60,7 @@ elif x==2:
         if i =="*" or i == "/":
             #STRING TEMPORAL CERO
             # TEMPORALCERO = VARIABLE | OPERANDO 1 | VARIABLE
-            temporalCero = "_t0 = " + p[suma-1] + " " +  p[suma] + " " + p[suma+1]  #LA LISTA P VAN DESGLOZANDO LA EXPRESION EN PARTES
-            #QUE P = [X,=,a,a,+,b,-,b]
+            temporalCero = "_t0 = " + p[suma-1] + " " +  p[suma] + " " + p[suma+1] 
         
    
     temporalUno = ""
@@ -81,7 +84,7 @@ elif x==3:
         if i != " ":
             p.append(i)
     temporalCero = ""
-    for i in p: # MULTIPLICACION O DIVISION
+    for i in p: 
         suma +=1
         if i =="(" or i == ")":
             #STRING TEMPORAL CERO
@@ -96,7 +99,7 @@ elif x==3:
                 temporalUno = "_t1 = " + p[suma-6] + " " +  p[suma-5] + " " +  "_t0"
     temporalDos = ""
     for i in p:
-        if i == "+" or i == "-": # SUMA O RESTA 
+        if i == "+" or i == "-": 
             if p[2] == "(" :
                 temporalDos = "_t2="+ " _t1 " + p[suma-1] + " " +  p[suma] + " " 
             else:
